b78238f441c2d31361322ac7e3e21e2226e62b8e
Sahana Bhat <SahanaBhat3388@gmail.com>
2023-01-30 12:27:53 +0530

    Implemented dynamic navigation menu using View Component

---
+++ ViewComponents/TutorialMenuViewComponent.cs
@@ -0,0 +1,23 @@
+﻿
+
+using Asp.netCoreMVCIntro.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.netCoreMVCIntro.ViewComponents
+{
+    public class TutorialMenuViewComponent:ViewComponent
+    {
+        private readonly ITutorialRepository _tutorialRepository;
+        public TutorialMenuViewComponent(ITutorialRepository tutorialRepository)
+        {
+            _tutorialRepository = tutorialRepository;
+        }
+
+        //Synchronous method
+        public IViewComponentResult Invoke()
+        {
+            var tutorials = _tutorialRepository.GetAllTutorial().OrderBy(p => p.Name);
+            return View(tutorials);
+        }
+    }
+}
